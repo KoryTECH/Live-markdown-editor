f5bd62b92cb26fb16c493ed82e298a99bbd33ddf
KoryTECH <korylinemedia@gmail.com>
2026-03-01 12:41:46 +0100

    Add initial HTML and JavaScript files for Scribe Markdown editor refactoring the entire project

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// get the main 3 screens
+const loadingPage = document.getElementById("welcomePage");
+const homePage = document.getElementById("editorHomePage");
+const markdownPage = document.getElementById("markdownInterface");
+
+homePage.classList.add("hidden");
+markdownPage.classList.add("hidden");
+// onload display loading welcome screen
+
+setTimeout(
+  function () {
+    loadingPage.classList.add("hidden");
+    homePage.classList.remove("hidden");
+    markdownPage.classList.remove("hidden");
+  },
+  5000
+);
+
+
