858ba3edc9679e8977dfe755ae84cd9623e6c053
KoryTECH <korylinemedia@gmail.com>
2026-03-02 12:55:52 +0100

    Refactor HTML structure and enhance JavaScript functionality for file creation and navigation

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 "use strict";
+// debugger
 
-// get the main 3 screens
+// get elements for easy access
 const loadingPage = document.getElementById("welcomePage");
 const homePage = document.getElementById("editorHomePage");
 const markdownPage = document.getElementById("markdownInterface");
-const newFileBtn = document.querySelectorALL()
+const inputFileName = document.getElementById("inputFileName");
+const fileName = document.getElementById("#fileName")
+const newFileButtons = document.querySelectorAll("#newFileButton, #newFile");
+
+
 
 homePage.classList.add("hidden");
 markdownPage.classList.add("hidden");
+
 // onload display loading welcome screen
 
-setTimeout(
-  function () {
-    loadingPage.classList.add("hidden");
-    homePage.classList.remove("hidden");
-    markdownPage.classList.remove("hidden");
-  },
-  5000
-);
+setTimeout(function () {
+  loadingPage.classList.add("hidden");
+  homePage.classList.remove("hidden");
+}, 5000);
 
-
-function createNewFilefunc () {
-
+function createNewFilefunc() {
+  loadingPage.classList.add("hidden");
+  homePage.classList.add("hidden");
+  markdownPage.classList.remove("hidden");
 }
+
+newFileButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    createNewFilefunc()
+  })
+});
+
+const files = []
+const file1 ={
+    id: 7323329,
+    name: "delete",
+    dateCreated: 17-3-2026,
+}
+files.push(file1)
+
+localStorage.setItem("files",files)
+localStorage.getItem("files")
